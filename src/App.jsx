232c7c234e6149eb8a00509aaf0ecae1df9c0a0d
balirwaalvin <sanyukalvin@gmail.com>
2026-02-04 11:38:07 +0300

    Debug blank screen: Remove framer-motion from App root to prevent opacity lock

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { motion } from 'framer-motion'
 import Sidebar from './components/Sidebar'
 import ChatInterface from './components/ChatInterface'
 import './App.css'
